64349fb7c934c85daa7c973bc173a33bb4689b2b
gulsro <glserozkan@gmail.com>
2024-05-27 14:13:28 +0200

    adding notes to header server

--- server.hpp
+++ server.hpp
@@ -11,6 +11,13 @@
 // The server will use the Host header from incoming HTTP requests
 // to determine which virtual host configuration to apply.
 
+// When trying to determine which server block to send a request to, Nginx will first try to decide based on 
+// the specificity of the listen directive using the following rules:
+//     -Nginx translates all “incomplete” listen directives by substituting missing values with their default values
+//     so that each block can be evaluated by its IP address and port.
+
+Nginx translates all “incomplete” listen directives by substituting missing values with their default values so that each block can be evaluated by its IP address and port.
+
 class Server:
 {
     private:
