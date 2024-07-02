2127f5ab0eff2b0f1d5e6658ddd2a43b192a4375
eunbi0308 <eunbicho1995@gmail.com>
2024-07-02 11:11:31 +0200

    -Ofast flag is added to optimizing compiling

--- src/response/HttpResponse.cpp
+++ src/response/HttpResponse.cpp
@@ -146,7 +146,9 @@ void    HttpResponse::setResource()
 		else
 			this->resource = "." + Server->getRoot() + this->Request->getURI();
 	}
-	std::cout << YELLOW << this->resource <<std::endl;
+	#ifdef FUNC
+		std::cout << YELLOW  << "RESOURCE : " << this->resource << DEFAULT << std::endl;
+	#endif
 }
 
 // void	sendResponse(int fd)
