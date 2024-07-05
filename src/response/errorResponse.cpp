5d7569676a8480753775b3c9b752520f754f1150
eunbi-bb <eunbicho1995@gmail.com>
2024-07-05 18:45:22 +0200

    try to print an arror page

--- src/response/errorResponse.cpp
+++ src/response/errorResponse.cpp
@@ -11,7 +11,18 @@ std::string	createErrorResponse(int code)
 		ostream << "HTTP/1.1 " << code << " " << returnStatusMessage(code) << "\r\n";
 		ostream << "Content-Length: 0\r\n";
  		ostream << "\r\n";
-
+		if (code >= 400 && code <= 501)
+		{
+			std::string	filename = "./error_pages/" + std::to_string(code) + ".html";
+			std::ifstream file(filename);
+			if (file.is_open())
+			{
+				std::ostringstream fileContent;
+				fileContent << file.rdbuf();
+				ostream << fileContent.str();
+				file.close();
+			}
+		}
 		return ostream.str();
 }
 // const char*	HttpResponse::ErrorCodeException::what() const throw()
