faa88b6e78fbe07abbebfe893b9427b2dd8b1473
gulser <glserozkan@gmail.com>
2024-07-09 09:37:17 +0200

    debugging execve

--- src/Cgi.cpp
+++ src/Cgi.cpp
@@ -99,14 +99,20 @@ std::string    Cgi::execCgi(){
         //output of cgi script will be written in pipe
         std::cout << MAG << "child process: "<< cgiFile << RES << std::endl;
         // char **argv = NULL;
-        char *argv[2] = {cgiFile, NULL};
+        //char *argv[2] = {cgiFile, NULL};
+        
+        //GUL ADDED THESE 2 LINES:
+        char python_path[] = "/usr/bin/python3";
+        char *argv[3] = {python_path, cgiFile.find_last_of('/') + 1, NULL};
         // char *env2[] = {NULL};
         close(pip[0]); 
         if (dup2(pip[1], STDOUT_FILENO) < 0) 
             perror("write pipe failed"); //error
-        if (execve(cgiFile, argv, env) < 0){
+        //if (execve(cgiFile, argv, env) < 0){
+        //GUL ADDED THIS LINE:
+        if (execve(argv[0], argv, env) < 0){
             perror("child");
-            write(2, "ERROR\n", 6);
+            write(2, "ERRORMAAA\n", 6);
             exit(1);
         }
     }
