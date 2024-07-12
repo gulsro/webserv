2f078f9ed5be71188c8af54313a04fadcc024c30
Yuka Itoh <yitoh@f1r2s14.codam.nl>
2024-07-12 11:15:51 +0200

    upload html and py is added

--- cgi_bin/upload.py
+++ cgi_bin/upload.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 
-import cgi, os
+import cgi, cgitb
+import os
+
+cgitb.enable()
+
+print ("Content-type:text/html\r\n")
+print()
 
 form = cgi.FieldStorage()
-
 # Get filename here
-fileitem = form['filename']
+fileitem = form["filename"]
 
 # Test if the file was uploaded
-if fileitem.filename:
-   open(os.getcwd() + '/cgi-bin/tmp/' + os.path.basename(fileitem.filename), 'wb').write(fileitem.file.read())
-   message = 'The file "' + os.path.basename(fileitem.filename) + '" was uploaded to ' + os.getcwd() + '/cgi-bin/tmp'
+if fileitem.file:
+   fn = os.path.basename(fileitem.file)
+   open(os.getcwd() + '/cgi-bin/tmp/' + fn, 'wb').write(fileitem.file.read())
+   message = 'The file "' + fn + '" was uploaded to ' + os.getcwd() + '/cgi-bin/tmp'
 else:
    message = 'Uploading Failed'
 
-print("Content-Type: text/html;charset=utf-8")
-print ("Content-type:text/html\r\n")
-print("<H1> " + message + " </H1>")
+# print("<H1> " + message + " </H1>")
+
+print ('''<!DOCTYPE html>
+Content-Type: text/html\n
+<html>
+<body>
+   <p>%s</p>
+</body>
+</html>
+''' % (message,))
