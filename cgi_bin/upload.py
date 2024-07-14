b8a4f6efff4e32edc6ce9979231d130f26e51872
eunbi0308 <eunbicho1995@gmail.com>
2024-07-14 20:50:14 +0200

    index.html and upload.py are modified

--- cgi_bin/upload.py
+++ cgi_bin/upload.py
@@ -9,7 +9,7 @@ cgitb.enable()
 UPLOAD_DIR = "./html/upload"
 message = ""
 upload_message = ""
-file_created = False;
+file_created = False
 status_code = "200 OK"
 
 if not os.path.exists(UPLOAD_DIR):
@@ -34,7 +34,7 @@ if request_method == "POST":
 				f.write(fileitem.file.read())
 			upload_message = "File" + f" {file_name} uploaded successfully!"
 			status_code = "201 Created"
-			file_created = True;
+			file_created = True
 	else:
 		upload_message = "No file was uploaded."
 		status_code = "400 Bad Request"
@@ -48,7 +48,7 @@ html_content = (f"""
 </head>
 <body>
   <h1>File Upload</h1>
-  <form enctype="multipart/form-data" action="moneybird.py" method="post">
+  <form enctype="multipart/form-data" action="upload.py" method="post">
     <label for="file">Upload File:</label>
     <input type="file" id="file" name="file">
     <br><br>
@@ -62,7 +62,7 @@ html_content = (f"""
 # Printing response message
 print(f"HTTP/1.1 {status_code}", end="\r\n")
 print("Content-Type: text/html", end="\r\n"), 
-print(f"Content-Lenght: {len(html_content)}", end="\r\n")
+print(f"Content-Length: {len(html_content)}", end="\r\n")
 if (file_created == True):
 	print (f"Location: {file_path}", end="\r\n")
 print("", end="\r\n")
